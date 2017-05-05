de36ff50cd87b5b91fef79bf0b4f06a13616afad
高大 <adventure@buxiaodeMacBook-Pro.local>
2017-05-05 19:45:31 +0800

    remove db.type warning

--- src/setupApp.js
+++ src/setupApp.js
@@ -2,3 +2,17 @@ import moment from 'moment';
 import 'moment/locale/zh-cn';
 
 moment.locale('zh-cn');
+
+// DEV
+// 移除 db.type 废弃警告
+/* eslint-disable */
+const PouchDB = require('pouchdb-core');
+/* eslint-enable */
+Object.defineProperty(PouchDB.prototype, '_remote', {
+  get() {
+    return typeof this.__remote === 'boolean' ? this.__remote : this.type() === 'http';
+  },
+  set(val) {
+    this.__remote = val;
+  }
+});
