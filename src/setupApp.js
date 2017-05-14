832c7dee89317f37efd928a4f4b067a28e395821
高大 <adventure@buxiaodeMacBook-Pro.local>
2017-05-14 21:20:12 +0800

    promise finally

--- src/setupApp.js
+++ src/setupApp.js
@@ -1,24 +1,41 @@
 import moment from 'moment';
 import 'moment/locale/zh-cn';
-import Promise from 'beauty-promise';
 
 moment.locale('zh-cn');
 
-global.Promise = Promise;
-
-Promise.onUnhandledRejection = function (reason) {
-  const msgs = [];
-  if (reason == null) {
-    msgs.push(String(reason));
-  } else if (typeof reason === 'object' && (reason.message || reason.stack)) {
-    if (reason.message) {
-      msgs.push(`message: ${reason.message}`);
-    }
-    if (reason.stack) {
-      msgs.push(`stack: ${reason.stack}`);
-    }
-  } else {
-    msgs.push(reason);
+/* eslint-disable no-extend-native */
+Promise.prototype.$finally = function onFinally(fnFinally) {
+  function doFinally() {
+    try {
+      fnFinally();
+    } catch (e) { /* pass */ }
   }
-  console.warn(`Possible Unhandled Promise Rejection: ${msgs.join('\n')}`);
+  this.then(doFinally, doFinally);
+  return this.then(
+    (value) => {
+      doFinally();
+      return value;
+    },
+    (reason) => {
+      doFinally();
+      return Promise.reject(reason);
+    }
+  );
 };
+
+// Promise.onUnhandledRejection = function (reason) {
+//   const msgs = [];
+//   if (reason == null) {
+//     msgs.push(String(reason));
+//   } else if (typeof reason === 'object' && (reason.message || reason.stack)) {
+//     if (reason.message) {
+//       msgs.push(`message: ${reason.message}`);
+//     }
+//     if (reason.stack) {
+//       msgs.push(`stack: ${reason.stack}`);
+//     }
+//   } else {
+//     msgs.push(reason);
+//   }
+//   console.warn(`Possible Unhandled Promise Rejection: ${msgs.join('\n')}`);
+// };
